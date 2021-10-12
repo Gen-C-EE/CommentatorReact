5b7c976d242946cf45cb404982b4c80777ccd699
attackTheChocha <conor-henry@uiowa.edu>
2021-10-12 16:26:13 -0500

    change to https

--- src/apis/videoApi.js
+++ src/apis/videoApi.js
@@ -2,7 +2,7 @@ const baseUrl="Commentatorspring-env.eba-8f3h3emw.us-east-2.elasticbeanstalk.com
 
 export const postVideo = (watchId) => {
     var newVideo = {watchID: watchId}
-    return fetch(`http://${baseUrl}/videos`,
+    return fetch(`https://${baseUrl}/videos`,
         {
             method: 'POST',
             body: JSON.stringify(newVideo),
